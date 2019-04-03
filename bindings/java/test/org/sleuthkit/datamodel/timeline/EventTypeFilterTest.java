039ca02a88134e88abced04c77306da080e39af2
millmanorama <millmanorama@gmail.com>
2019-04-03 12:07:45 +0200

    simplify test code

--- bindings/java/test/org/sleuthkit/datamodel/timeline/EventTypeFilterTest.java
+++ bindings/java/test/org/sleuthkit/datamodel/timeline/EventTypeFilterTest.java
@@ -33,26 +33,8 @@ public class EventTypeFilterTest {
 	public EventTypeFilterTest() {
 	}
 
-	@BeforeClass
-	public static void setUpClass() {
-	}
 
-	@AfterClass
-	public static void tearDownClass() {
-	}
-
-	@Before
-	public void setUp() {
-	}
-
-	@After
-	public void tearDown() {
-	}
-
-	// @Test
-	public void testDisplayName() {
-
-	}
+	
 
 	/**
 	 * Test of getEventType method, of class EventTypeFilter.
--- bindings/java/test/org/sleuthkit/datamodel/timeline/RootFilterTest.java
+++ bindings/java/test/org/sleuthkit/datamodel/timeline/RootFilterTest.java
@@ -39,21 +39,6 @@ public class RootFilterTest {
 	public RootFilterTest() {
 	}
 
-	@BeforeClass
-	public static void setUpClass() {
-	}
-
-	@AfterClass
-	public static void tearDownClass() {
-	}
-
-	@Before
-	public void setUp() {
-	}
-
-	@After
-	public void tearDown() {
-	}
 
 	/**
 	 * Test of copyOf method, of class TimelineFilter.
@@ -86,5 +71,4 @@ public class RootFilterTest {
 				emptyList);
 
 	}
-
 }
